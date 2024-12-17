f5e95882b070e5a07132d4c97400ba06ebd4ada1
samork26 <vohra207@gmail.com>
2024-12-17 11:26:33 -0500

    filler content

---
+++ script.js
@@ -0,0 +1,14 @@
+// Simple smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
